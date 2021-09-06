92b5a92301dc53883f8211d8ef3938c727a0585f
zhaohu xing <920232796@qq.com>
2021-09-06 20:18:46 +0800

    添加了evaluation文件

---
+++ medical_seg/evaluation/brats.py
@@ -0,0 +1,76 @@
+
+
+from medpy.metric import dc, hd95
+import numpy as np 
+
+
+def compute_BraTS_dice(ref, pred):
+    """
+    ref and gt are binary integer numpy.ndarray s
+    :param ref:
+    :param gt:
+    :return:
+    """
+    num_ref = np.sum(ref)
+    num_pred = np.sum(pred)
+
+    if num_ref == 0:
+        if num_pred == 0:
+            return 1
+        else:
+            return 0
+    else:
+        return dc(pred, ref)
+
+def compute_BraTS_HD95(ref, pred):
+    """
+    ref and gt are binary integer numpy.ndarray s
+    spacing is assumed to be (1, 1, 1)
+    :param ref:
+    :param pred:
+    :return:
+    """
+    num_ref = np.sum(ref)
+    num_pred = np.sum(pred)
+
+    if num_ref == 0:
+        if num_pred == 0:
+            return 0
+        else:
+            return 373.12866
+    elif num_pred == 0 and num_ref != 0:
+        return 373.12866
+    else:
+        return hd95(pred, ref, (1, 1, 1))
+
+
+def evaluate_BraTS_case(arr: np.ndarray, arr_gt: np.ndarray):
+    """
+    attempting to reimplement the brats evaluation scheme
+    assumes edema=1, non_enh=2, enh=3
+    :param arr:
+    :param arr_gt:
+    :return:
+    """
+    # whole tumor
+    mask_gt = (arr_gt != 0).astype(int)
+    mask_pred = (arr != 0).astype(int)
+    dc_whole = compute_BraTS_dice(mask_gt, mask_pred)
+    hd95_whole = compute_BraTS_HD95(mask_gt, mask_pred)
+    del mask_gt, mask_pred
+
+    # tumor core
+    mask_gt = (arr_gt > 1).astype(int)
+    mask_pred = (arr > 1).astype(int)
+    dc_core = compute_BraTS_dice(mask_gt, mask_pred)
+    hd95_core = compute_BraTS_HD95(mask_gt, mask_pred)
+    del mask_gt, mask_pred
+
+    # enhancing
+    mask_gt = (arr_gt == 3).astype(int)
+    mask_pred = (arr == 3).astype(int)
+    dc_enh = compute_BraTS_dice(mask_gt, mask_pred)
+    hd95_enh = compute_BraTS_HD95(mask_gt, mask_pred)
+    del mask_gt, mask_pred
+
+    return dc_whole, dc_core, dc_enh, hd95_whole, hd95_core, hd95_enh
---
+++ medical_seg/evaluation/evaluation.py
@@ -0,0 +1,35 @@
+from collections import OrderedDict
+from general import ConfusionMatrix, ALL_METRICS
+import numpy as np 
+
+class Metric:
+
+    def __init__(self, class_name=[], voxel_spacing=(1, 1, 1), nan_for_nonexisting=True) -> None:
+        ## 不计算背景，因此把从1开始
+        assert len(class_name) != 0, "请传入class_name参数。"
+        self.class_num = len(class_name) + 1
+        self.nan_for_nonexisting = nan_for_nonexisting
+        self.class_name = class_name
+        self.voxel_spacing = voxel_spacing
+        
+    def run(self, pred, label):
+        # pred为argmax以后的结果
+        res = OrderedDict({f"{c}_{m}": 0.0 for c in self.class_name for m in ALL_METRICS})
+
+        for c in range(self.class_num):
+            if c == 0:
+                continue
+            c_name = self.class_name[c - 1]
+            for m_name, m_func in ALL_METRICS.items():
+                res[f"{c_name}_{m_name}"] = m_func(test=(pred == c), reference=(label == c), 
+                                                    confusion_matrix=None, nan_for_nonexisting=self.nan_for_nonexisting, 
+                                                    voxel_spacing=self.voxel_spacing)
+        return res
+
+if __name__ == "__main__":
+    m = Metric(class_name=["1", "2", "3"])
+
+    a1 = np.random.rand(1, 32, 32) > 0.5
+    a2 = np.random.rand(1, 32, 32) > 0.5
+
+    print(m.run(a1, a2))
